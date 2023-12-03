cb83e6d812ad3c76b3d111c9734b7de190fd48ed
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2023-12-03 22:45:00 +0700

    android studio

---
+++ SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/MainActivity.java
@@ -0,0 +1,151 @@
+package com.komputerkit.sqlitedatabase;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Database db;
+    EditText etBarang, etStok, etHarga;
+    TextView tvPilihan;
+    List<Barang> databarang = new ArrayList<Barang>();
+    BarangAdapter adapter;
+    RecyclerView rcvBarang;
+    String idbarang;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        load();
+        selectData();
+    }
+
+    public void load(){
+        db = new Database(this);
+
+        db.buatTabel();
+
+        etBarang = findViewById(R.id.etBarang);
+        etStok = findViewById(R.id.etStok);
+        etHarga = findViewById(R.id.etHarga);
+        tvPilihan = findViewById(R.id.tvPilihan);
+        rcvBarang = findViewById(R.id.rcvBarang);
+
+        rcvBarang.setLayoutManager(new LinearLayoutManager(this));
+        rcvBarang.setHasFixedSize(true);
+    }
+
+    public void simpan (View v){
+        String barang = etBarang.getText().toString();
+        String stok = etStok.getText().toString();
+        String harga = etHarga.getText().toString();
+        String pilihan = tvPilihan.getText().toString();
+
+        if (barang.isEmpty() || stok.isEmpty() || harga.isEmpty()) {
+            pesan("Data Kosong");
+        } else {
+            if (pilihan.equals("insert")){
+                String sql = "INSERT INTO tblbarang (barang,stok,harga) VALUES ('"+barang+"',"+stok+","+harga+")";
+                pesan(sql);
+
+                if (db.runSQL(sql)){
+                    pesan("insert berhasil");
+                    selectData();
+                } else {
+                    pesan("insert gagal");
+                }
+            } else {
+                pesan("update");
+            }
+        }
+        etBarang.setText("");
+        etStok.setText("");
+        etHarga.setText("");
+        tvPilihan.setText("insert");
+    }
+
+    public  void pesan (String isi){
+        Toast.makeText(this, isi, Toast.LENGTH_SHORT).show();
+    }
+
+    public void selectData (){
+        String sql = "SELECT * FROM tblbarang ORDER BY barang ASC";
+        Cursor cursor = db.select(sql);
+        databarang.clear();
+
+        if (cursor.getCount() > 0){
+            while (cursor.moveToNext()){
+                String idbarang = cursor.getString(cursor.getColumnIndex("idbarang"));
+                String barang = cursor.getString(cursor.getColumnIndex("barang"));
+                String stok = cursor.getString(cursor.getColumnIndex("stok"));
+                String harga = cursor.getString(cursor.getColumnIndex("harga"));
+
+                databarang.add(new Barang(idbarang,barang,stok,harga));
+            }
+
+            adapter = new BarangAdapter(this,databarang);
+            rcvBarang.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+
+        }else {
+            pesan("Data Kosong");
+        }
+    }
+
+    public void deleteData(String id){
+        idbarang = id;
+
+        AlertDialog.Builder al = new AlertDialog.Builder(this);
+        al.setTitle("Peringatan!");
+        al.setMessage("Yakin akan menghapus?");
+        al.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String sql = "DELETE FROM tblbarang WHERE idbarang = "+idbarang+"";
+                    if (db.runSQL(sql)){
+                        pesan("Data sudah dihapus");
+                        selectData();
+                    } else {
+                        pesan("Data tidak bisa dihapus");
+                    }
+            }
+        });
+
+        al.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        al.show();
+    }
+
+    public void selectUpdate(String id){
+        idbarang = id;
+        String sql = "SELECT * FROM tblbarang WHERE idbarang = "+id+"";
+        Cursor cursor = db.select(sql);
+        cursor.moveToNext();
+
+        etBarang.setText(cursor.getString(cursor.getColumnIndex("barang")));
+        etStok.setText(cursor.getString(cursor.getColumnIndex("stok")));
+        etHarga.setText(cursor.getString(cursor.getColumnIndex("harga")));
+
+        tvPilihan.setText("update");
+    }
+}
